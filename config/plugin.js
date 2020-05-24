8e4a4ee04af263d7a8b63904e3cd16efc05216fb
Joz <jozdortraz@gmail.com>
2020-05-24 15:04:06 +0800

    feat: update typings

--- config/plugin.js
+++ config/plugin.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const plugin = {
-    sequelize: {
-        enable: true,
-        package: 'egg-sequelize',
-    },
-    redis: {
-        enable: true,
-        package: 'egg-redis',
-    },
-    alinode: {
-        enable: process.env.NODE_ENV === 'production',
-        package: 'egg-alinode',
-    },
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  },
+  redis: {
+    enable: true,
+    package: 'egg-redis',
+  },
+  alinode: {
+    enable: process.env.NODE_ENV === 'production',
+    package: 'egg-alinode',
+  },
 };
 
 module.exports = exports = plugin;
--- index.d.ts
+++ index.d.ts
@@ -9,10 +9,10 @@ type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? R
   ? InstanceType<U>
   : U;
 
-import ExportCoreCache from './app/service/core/cache';
-import ExportCoreHttp from './app/service/core/http';
-import ExportCoreShortId from './app/service/core/shortid';
-import ExportCoreAccount from './app/service/core/account';
+import ExportCoreCache = require('./app/service/core/cache');
+import ExportCoreHttp = require('./app/service/core/http');
+import ExportCoreShortId = require('./app/service/core/shortid');
+import ExportCoreAccount = require('./app/service/core/account');
 declare module 'egg' {
   interface Application {
     redis: Redis;
