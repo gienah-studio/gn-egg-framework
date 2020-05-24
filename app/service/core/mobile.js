6e0786c1872e6fdd2c7a5913d9ef3a2a4de47788
Joz <jozdortraz@gmail.com>
2020-05-24 18:50:32 +0800

    feat: add mobile service

---
+++ app/service/core/mobile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { Service } = require('egg');
+const { GN_ERROR_CODE, default: GNError } = require('gn-egg-lib/gn-error');
+const _ = require('lodash');
+
+class MobileService extends Service {
+  async sendCaptcha(mobile, sceneCode) {
+    const { service } = this;
+
+    try {
+      const res = await service.core.http.post(`http://${process.env.MOBILE_SVC_HOST}/captcha/send`, { mobile, sceneCode });
+      return res;
+    } catch (e) {
+      throw new GNError(GN_ERROR_CODE.MOBILE_CAPTCHA_SENT_ERROR, e);
+    }
+  }
+}
+
+module.exports = MobileService;
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 import * as Egg from 'egg';
-import { Application } from 'egg';
 import { Redis } from 'ioredis';
 
 type AnyClass = new (...args: any[]) => any;
@@ -13,6 +12,7 @@ import ExportCoreCache = require('./app/service/core/cache');
 import ExportCoreHttp = require('./app/service/core/http');
 import ExportCoreShortId = require('./app/service/core/shortid');
 import ExportCoreAccount = require('./app/service/core/account');
+import ExportCoreMobile = require('./app/service/core/mobile');
 declare module 'egg' {
   interface Application {
     redis: Redis;
@@ -23,6 +23,7 @@ declare module 'egg' {
       http: AutoInstanceType<typeof ExportCoreHttp>;
       shortid: AutoInstanceType<typeof ExportCoreShortId>;
       account: AutoInstanceType<typeof ExportCoreAccount>;
+      mobile: AutoInstanceType<typeof ExportCoreMobile>;
     };
   }
 }
