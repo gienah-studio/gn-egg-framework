b1e8874926cbe57cc162cb6fac0862a6a035c474
Jozdortraz <jozdortraz@gmail.com>
2020-08-03 09:44:18 +0800

    feat: add config to http service

--- app/service/core/http.js
+++ app/service/core/http.js
@@ -8,13 +8,15 @@ class HttpService extends Service {
    * Call http get method
    * @param {string} url url to get
    * @param {object} params get params
+   * @param {object} options httpClient options
    */
-  async get(url, params) {
+  async get(url, params, options) {
     const { ctx } = this;
     try {
       return await ctx.curl(url, {
         data: params,
         dataType: 'json',
+        ...options,
       });
     } catch (e) {
       throw new GNError(GN_ERROR_CODE.HTTP_GET_ERROR, {
@@ -29,8 +31,9 @@ class HttpService extends Service {
    * Call http post method
    * @param {string} url url to post
    * @param {object} params post params
+   * @param {object} options httpClient options
    */
-  async post(url, params) {
+  async post(url, params, options) {
     const { ctx } = this;
     try {
       return await ctx.curl(url, {
@@ -38,6 +41,7 @@ class HttpService extends Service {
         contentType: 'json',
         method: 'POST',
         dataType: 'json',
+        ...options,
       });
     } catch (e) {
       throw new GNError(GN_ERROR_CODE.HTTP_POST_ERROR, {
